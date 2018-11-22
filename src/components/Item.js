dd9975efdbed56b0e6b9b8dcd4ed59abae760368
Josh Goldberg <joshuapgoldberg@gmail.com>
2018-11-22 11:19:02 -0500

    Added Expand/Collapse

--- src/components/Item.js
+++ src/components/Item.js
@@ -8,25 +8,40 @@ export class Item extends Component {
 		this.state = {
 			expanded:false
 		}
+		this.handleExpand = this.handleExpand.bind(this);
+	}
+
+	handleExpand(){
+		this.setState(prevState => ({
+			expanded: !prevState.expanded
+		  }));
 	}
 
 	render() {
 		const subs = this.props.subs;
+		let subContainer, subItems, icon;
 
-		subs.length === 0 ? createSubItems : console.log('subs');
-
+		if (subs.length){
+			subItems = subs.map((label, i) =>
+			<SubItem
+				key={i}
+				index={i}
+				left={label.category}
+				right={label.amount}
+			/>);
+			subContainer = <div className="sub-items">{subItems}</div>;
+			icon = <div className={this.state.expanded ? "icon-collapse":"icon-expand"}></div>;
+		}
 		return (
-			<div className="Item">
-				<div className="col-left">
-				<div className="icon icon-collapse"></div>
+			<div className={this.state.expanded ? 'Item-expand': 'Item'} onClick={this.props.subs.length ? this.handleExpand: null}>
+				<div className={this.props.subs.length ? "col-left" : "col-left-no-icon"}>
+				{icon}
 				{this.props.left}
 				</div>
 				<div className="col-right">
 				{this.props.right}
 				</div>
-				<div>
-					
-				</div>
+				{subContainer}
 			</div>
 		)
 	}
